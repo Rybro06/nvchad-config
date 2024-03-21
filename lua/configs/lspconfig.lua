618e37aaa121814436a3a21befa02fd3fd29ed34
Ryan Brown <ryan.brown@romariccorp.com>
2024-03-21 10:17:52 -0700

    chore(init): migrate plugins from v2.0

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,10 +1,34 @@
--- EXAMPLE 
 local on_attach = require("nvchad.configs.lspconfig").on_attach
 local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls" }
+local servers = {
+	-- web dev stuff
+	"cssls",
+	"html",
+	"tsserver",
+	"jsonls",
+
+	-- c/cpp stuff
+	"clangd",
+	"lemminx",
+
+	-- rust
+	"rust_analyzer",
+	"taplo",
+	"wgsl_analyzer",
+
+	-- python
+	"pyright",
+
+	-- docker
+	"bashls",
+	"docker_compose_language_service",
+	"dockerls",
+	"nil_ls",
+	"yamlls",
+}
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
@@ -15,9 +39,9 @@ for _, lsp in ipairs(servers) do
   }
 end
 
--- typescript
-lspconfig.tsserver.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-}
+-- Example custom setup for one server
+-- lspconfig.tsserver.setup {
+--   on_attach = on_attach,
+--   on_init = on_init,
+--   capabilities = capabilities,
+-- }
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,10 +1,26 @@
 require "nvchad.mappings"
 
--- add yours here
+-- General Keymap Changes
+-- shortcuts
+vim.keymap.set("n", "<leader>q", "<Cmd>q<CR>", { desc = "General Quit Vim Window" })
+vim.keymap.set("n", ";", ":", { desc = "General Enter command mode" })
 
-local map = vim.keymap.set
+-- cursor stability
+vim.keymap.set("n", "J", "mzJ`z", { desc = "General Join lines" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "General Half page up" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "General Half page down" })
+vim.keymap.set("n", "n", "nzzzv", { desc = "General Next search result" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "General Previous search result" })
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+-- whitespace toggle
+vim.keymap.set("n", "<leader>ts", "<Cmd>set list!<CR>", { desc = "General Toggle whitespace visibility" })
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- shift selection
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "General Shift selection down" })
+vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv", { desc = "General Shift selection down" })
+
+-- Move between windows
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Focus Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Focus Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Focus Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Focus Move focus to the upper window" })
